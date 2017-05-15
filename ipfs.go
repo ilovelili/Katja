48da1536b2e3f4aedc12546087a3852d94fdb4bd
mju <mju@cimpress.com>
2017-05-15 15:51:26 +0900

    ipfs wrapper

--- ipfs.go
+++ ipfs.go
@@ -5,14 +5,26 @@ import (
 	"fmt"
 	cid "gx/ipfs/QmYhQaCYEcaPPjxJX7YcPcVKkQfRy6sJ7B3XmGFk82XYdQ/go-cid"
 
+	"runtime"
+
 	"github.com/ipfs/go-ipfs/core"
+	"github.com/ipfs/go-ipfs/merkledag"
+	"github.com/ipfs/go-ipfs/path"
 	"github.com/ipfs/go-ipfs/repo/fsrepo"
 )
 
-// StartNode Start Node
-func StartNode(repo string) (*core.IpfsNode, error) {
+func defaultPath() string {
+	if runtime.GOOS == "windows" {
+		return "C:\\Users\\username\\.ipfs"
+	}
+	return "~/.ipfs"
+}
+
+// StartNode Start IPFS Node
+func StartNode() (*core.IpfsNode, error) {
 	// Assume the user has run 'ipfs init'
-	r, err := fsrepo.Open(repo) // "~/.ipfs" in linux and "C:\Users\username\.ipfs" in windows
+	repo := defaultPath()
+	r, err := fsrepo.Open(repo)
 	if err != nil {
 		return nil, err
 	}
@@ -28,46 +40,93 @@ func StartNode(repo string) (*core.IpfsNode, error) {
 	return core.NewNode(ctx, cfg)
 }
 
-// GetStrings get strings by userID
-func GetStrings(node *core.IpfsNode, userID string) ([]string, error) {
-	// base58: https://kikobeats.com/base58-is-base64-for-humans/
-	cid, err := cid.Decode(userID)
-	if err != nil {
-		return nil, err
-	}
-
-	return resolveAllInOrder(node, cid), nil
-}
-
-func resolveAllInOrder(nd *core.IpfsNode, cid *cid.Cid) (stringArr []string) {
+// GetStrings get strings by cid
+func GetStrings(node *core.IpfsNode, cid *cid.Cid) (stringArr []string, err error) {
 	ctx, cancel := context.WithCancel(context.Background())
+	nodeGetter := node.DAG
 	defer cancel()
-	node, err := nd.DAG.Get(ctx, cid)
-	fmt.Println("the node is", node)
+	// merkledag proto Node
+	nd, err := nodeGetter.Get(ctx, cid)
+	fmt.Println("the node is", nd)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
 	fmt.Printf("bout to crash")
-	fmt.Printf("%s", string(node.RawData()[:]))
+	fmt.Printf("%s", nd.String())
 	fmt.Println("not crashed ")
 
 	for {
 		var err error
-		if len(node.Links()) == 0 {
+		if len(nd.Links()) == 0 {
 			break
 		}
 
-		node, err = node.Links()[0].GetNode(ctx, nd.DAG)
+		nd, err = nd.Links()[0].GetNode(ctx, nodeGetter)
 		if err != nil {
 			fmt.Println(err)
+			break
 		}
 
-		data := string(node.RawData()[:])
+		data := nd.String()
 		fmt.Println(data)
 		stringArr = append(stringArr, data)
 	}
 
-	return stringArr
+	return stringArr, nil
+}
+
+// AddString add input string to ipfs node
+func AddString(node *core.IpfsNode, inputString string) (*cid.Cid, error) {
+	pointsTo, err := node.Namesys.Resolve(node.Context(), node.Identity.Pretty())
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	//If there is an error, user is new and hasn't yet created a DAG.
+	if err != nil {
+		newProtoNode := makeStringNode(inputString)
+		cid, err := node.DAG.Add(newProtoNode)
+		if err != nil {
+			return nil, err
+		}
+
+		err = node.Namesys.Publish(ctx, node.PrivateKey, path.FromCid(cid))
+		if err != nil {
+			return nil, err
+		}
+
+		return cid, nil
+	}
+
+	// Else user has already creatd a DAG
+	newProtoNode := makeStringNode(inputString)
+	cid, err := cid.Decode(pointsTo.String())
+	if err != nil {
+		return nil, err
+	}
+
+	oldProtoNode, err := node.DAG.Get(ctx, cid)
+	if err != nil {
+		return nil, err
+	}
+
+	err = newProtoNode.AddNodeLink("next", oldProtoNode)
+	if err != nil {
+		return nil, err
+	}
+
+	node.DAG.Add(newProtoNode)
+	err = node.Namesys.Publish(ctx, node.PrivateKey, pointsTo)
+	if err != nil {
+		return nil, err
+	}
+
+	return cid, nil
+}
+
+func makeStringNode(s string) *merkledag.ProtoNode {
+	nd := new(merkledag.ProtoNode)
+	nd.SetData([]byte(s))
+	return nd
 }
